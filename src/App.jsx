70653f5524ae926d83cf0324cd57cad31047a714
Bakshish Singh <bakshish10621@gmail.com>
2023-09-21 15:45:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffers from "./sections/SpecialOffers";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+import Hero from "./sections/hero";
+
+const App = ()=> {
+  return (
+    <main className='relative'>
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffers />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
+  
+  export default App
